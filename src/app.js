c9deac7c7e860c67edf617ab3df2f13c1a00dac2
veloturistos <56963469+veloturistos@users.noreply.github.com>
2023-03-22 16:31:11 +0300

    comments to limitations

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const {sequelize} = require('./model')
 const {getProfile} = require('./middleware/getProfile')
 const router = require('./router/router');
 
+//logger for with formatinng. in the production change loglevel and add logging in the required places
 const log4js = require('log4js');
 log4js.configure({
     appenders: { 'logging': { type: 'console' } },
@@ -16,12 +17,14 @@ app.use(bodyParser.json());
 app.set('sequelize', sequelize)
 app.set('models', sequelize.models)
 
+//all testing was done with Postman due to time limitations
 app.use(router);
 
 app.use(function(req, res) {
   return res.status(404).send({url: req.originalUrl + ' not found'})
 });
 
+// simple error processing, for production purpose can be upgraded 
 app.use(function(err, req, res, next) {
   logger.error(err.code + ' ' + err.message);
   res.status(err.code)
